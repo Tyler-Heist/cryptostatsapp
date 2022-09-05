d8c5cdf4a8a9e664886c5ce263174f1becd2603f
Tyler Heist <tyler.heist@snhu.edu>
2022-09-05 01:10:06 -0400

    Initial Commit

--- application.py
+++ application.py
@@ -6,13 +6,13 @@ from flask import Flask, render_template
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
+@application.route('/')
 # ‘/’ URL is bound with index() function.
 def index():
     recent_stats = btcPriceSite.most_recent_stats()
@@ -36,4 +36,4 @@ def index():
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(debug=True)
+    application.run(debug=True)
