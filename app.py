3bb82aea08aa106df31a155afba72b1463965704
Tyler-Heist <tlrhst3@gmail.com>
2025-06-18 23:59:03 -0400

    refactor: update Flask app logic refactor: update google_sheets_api call to accommodate changes in get_data function feat: add caching to reduce Google Sheet API calls from once per page load to once per hour

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from pathlib import Path
-
 from flask import Flask, render_template
-
+from flask_caching import Cache
+from pathlib import Path
 import google_sheets_api
 
 SHEET_ID = '1OKWn63iR-B9nxYuqebhIDhiasZWOT-61gUeoJkq8dsQ'
 THIS_FOLDER = Path(__file__).parent.resolve()
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+
+# Flask constructor takes the name of current module (__name__) as argument.
 app = Flask(__name__, template_folder=str(THIS_FOLDER / 'templates'))
+cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 3600})
 
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call the associated function.
 @app.route('/')
+@cache.cached()
 def index():
-    response = google_sheets_api.get_values(sheet_id=SHEET_ID, sheet_range='STATS!A1:I')
+    response = google_sheets_api.get_data(spreadsheet_id=SHEET_ID, spreadsheet_range='Stats!A1:I', df_bool=False)
     recent_stats = response[-1]
     return render_template('index.html', days_running=len(response)-1, data_from=recent_stats[0],
                            low_time=recent_stats[1], low_price=recent_stats[2], low_mode=recent_stats[3],
@@ -27,4 +26,4 @@ def index():
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(debug=True)
+    app.run(debug=False)
