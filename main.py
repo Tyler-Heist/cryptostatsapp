2b2e7208b1c439b70e45e8632bdc3743f5d1b28c
Tyler-Heist <tlrhst3@gmail.com>
2025-12-09 21:42:07 -0500

    refactor - put api call in get_price_data function

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from logging.handlers import RotatingFileHandler
-from tzlocal import get_localzone
-from dotenv import load_dotenv
-import google_sheets_api
-import pandas as pd
 import datetime
-import requests
-import schedule
 import logging
+import os
 import sys
 import time
-import os
-from pushbullet import Pushbullet
+from logging.handlers import RotatingFileHandler
 
+import pandas as pd
+import requests
+import schedule
+from dotenv import load_dotenv
+from pushbullet import Pushbullet
+from tzlocal import get_localzone
+
+import google_sheets_api
 
 handler = RotatingFileHandler('cryptostatsapp.log', maxBytes=5*1024*1024, backupCount=5)
 logging.basicConfig(
@@ -28,8 +29,37 @@ PRICE_DATA_SHEET = 'Price_Data!A1:D'
 PREVIOUS_DAY_STATS_SHEET = 'Previous_Day_Stats!A:E'
 STATS_SHEET = 'Stats!A1:I'
 
+GSHEET_API_KEY = os.getenv('API_KEY')
 PUSHBULLET_TOKEN = os.getenv('PUSHBULLET_TOKEN')
 pb = Pushbullet(PUSHBULLET_TOKEN)
+
+
+def get_price_data() -> float | None:
+    # Parameters for API call
+    params = {
+        'market': 'coinbase',
+        'instruments': 'BTC-USD',
+        'apply_mapping': 'true',
+        'api_key': GSHEET_API_KEY
+    }
+
+    # Get current BTC price coindesk.com via API (gets data from Coinbase)
+    logging.info('Calling coindesk API')
+    try:
+        response = requests.get(url='https://data-api.coindesk.com/spot/v1/latest/tick', params=params, timeout=3)
+        response.raise_for_status()
+        response_data = response.json()
+        current_price = response_data.get('Data', {}).get('BTC-USD', {}).get('PRICE', '')
+        if not current_price:
+            logging.warning(f'Price data not found: {response_data} - Status Code: {response.status_code}')
+            return None
+        return current_price
+    except requests.exceptions.RequestException as e:
+        logging.exception(f'An error occurred: {e}')
+        raise e
+    except ValueError as e:
+        logging.exception(f'JSON error: {e}')
+        raise e
 
 
 def main() -> None:
@@ -38,28 +68,7 @@ def main() -> None:
 
         pb.push_note(title='CryptoStatsApp', body='Script Started')
 
-        # Parameters for API call
-        params = {
-            'market': 'coinbase',
-            'instruments': 'BTC-USD',
-            'apply_mapping': 'true',
-            'api_key': os.getenv('API_KEY')
-        }
-
-        # Get current BTC price coindesk.com via API (gets data from Coinbase)
-        logging.info('Calling coindesk API')
-        current_price = 0
-        try:
-            response = requests.get('https://data-api.coindesk.com/spot/v1/latest/tick', params=params, timeout=3)
-            response.raise_for_status()
-            response_data = response.json()
-            current_price = response_data.get('Data', {}).get('BTC-USD', {}).get('PRICE', '')
-            if not current_price:
-                logging.warning(f'Price data not found: {response_data} - Status Code: {response.status_code}')
-        except requests.exceptions.RequestException as e:
-            logging.exception(f'An error occurred: {e}')
-        except ValueError as e:
-            logging.exception(f'JSON error: {e}')
+        current_price = get_price_data()
 
         # Get date info
         now = datetime.datetime.now()
@@ -151,7 +160,7 @@ def main() -> None:
 # used to avoid execution when file is imported into another file
 if __name__ == "__main__":
     try:
-        main()
+        # main()
         schedule.every().hour.at('01:00').do(main)
         while True:
             schedule.run_pending()
