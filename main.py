3799da92bbdb5f2777c9e96be1763e1613b16cc1
Tyler-Heist <tlrhst3@gmail.com>
2025-12-17 18:23:09 -0500

    update logging and add pushbullet notification

--- main.py
+++ main.py
@@ -82,74 +82,10 @@ def main() -> None:
             logging.info(f'Appending data to: {SPREADSHEET_ID}')
             google_sheets_api.append_rows(data=data, spreadsheet_id=SPREADSHEET_ID, spreadsheet_range=PRICE_DATA_SHEET)
         except Exception as e:
-            logging.error(
-                f'Error appending to: {SPREADSHEET_ID} - Range: {PRICE_DATA_SHEET} - Data: {data} - Error: {e}'
-            )
+            msg = f'Error appending to sheet - Range: {PRICE_DATA_SHEET} - Data: {data} - Error: {e}'
+            logging.error(msg)
+            pb.push_note(title='CryptoStatsApp Error', body=msg)
             return
-
-        start_time = datetime.time(0, 0, 0)
-        end_time = datetime.time(0, 59, 59)
-
-        # Only execute the code below if the time is between midnight and 1AM
-        if start_time <= now.time() <= end_time:
-            logging.info('Getting all values from Previous_Day_Stats Google Sheet')
-            try:
-                previous_day_stats = google_sheets_api.get_data(
-                    spreadsheet_id=SPREADSHEET_ID,
-                    spreadsheet_range=PREVIOUS_DAY_STATS_SHEET
-                )
-            except Exception as e:
-                logging.error(f'Error getting data from: {SPREADSHEET_ID} - Error: {e}')
-                return
-
-            # New row to add to df before calculating most up-to-date mode
-            new_row = {
-                'Date': previous_day_stats['Date'][0],
-                'Low Time': previous_day_stats['Low Time'][0][:5],
-                'Low Price': previous_day_stats['Low Price'][0],
-                'Most Frequent Low': '',
-                'Low Accuracy': '=COUNTIF(INDIRECT("D2:D" & ROW()), INDIRECT("D" & ROW()))/ROW()',
-                'High Time': previous_day_stats['High Time'][0][:5],
-                'High Price': previous_day_stats['High Price'][0],
-                'Most Frequent High': '',
-                'High Accuracy': '=COUNTIF(INDIRECT("H2:H" & ROW()), INDIRECT("H" & ROW()))/ROW()'
-            }
-
-            logging.info(f'Get all daily stats from {SPREADSHEET_ID}')
-            try:
-                stats = google_sheets_api.get_data(spreadsheet_id=SPREADSHEET_ID, spreadsheet_range=STATS_SHEET)
-            except Exception as e:
-                logging.error(f'Error getting data from: {SPREADSHEET_ID} - Error: {e}')
-                return
-
-            logging.info('Adding new row to stats df')
-            stats.loc[len(stats)] = new_row
-
-            logging.info('Calculating mode of Most Frequent Low and Most Frequent High columns')
-            modes = stats.mode()
-            low_mode = modes['Most Frequent Low'].tolist()
-            high_mode = modes['Most Frequent High'].tolist()
-
-            logging.info('Adding modes into new_row')
-            new_row_index = len(stats) - 1
-            stats.at[new_row_index, 'Most Frequent Low'] = f"'{low_mode[0]}" if low_mode else 'N/A'
-            stats.at[new_row_index, 'Most Frequent High'] = f"'{high_mode[0]}" if high_mode else 'N/A'
-
-            logging.info('Converting last row in df (new_row) back into a list')
-            row_to_add = stats.iloc[new_row_index].tolist()
-
-            logging.info(f'Appending {new_row} to: {SPREADSHEET_ID}')
-            try:
-                google_sheets_api.append_rows(
-                    data=row_to_add,
-                    spreadsheet_id=SPREADSHEET_ID,
-                    spreadsheet_range=STATS_SHEET
-                )
-            except Exception as e:
-                logging.error(
-                    f'Error appending to: {SPREADSHEET_ID} - Range: {STATS_SHEET} - Data: {data} - Error: {e}'
-                )
-                return
     except Exception as e:
         logging.exception(f'Unhandled exception: {e}')
     finally:
