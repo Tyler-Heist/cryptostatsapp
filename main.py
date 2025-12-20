24e35bd86c67711e6d52881770228c653931db5c
Tyler-Heist <tlrhst3@gmail.com>
2025-12-20 04:30:18 -0500

    refactor: improve reliability put write logic in separate function add retry logic to get_price_data and write_price_data add guard so price isn't written if it is None move Pushbullet notification so it only sends on script restart

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import sys
 import time
 from logging.handlers import RotatingFileHandler
 
-import pandas as pd
 import requests
 import schedule
+from tenacity import retry, wait_fixed, RetryError
 from dotenv import load_dotenv
 from pushbullet import Pushbullet
+from tenacity import stop_after_attempt
 from tzlocal import get_localzone
 
 import google_sheets_api
@@ -32,6 +33,7 @@ PUSHBULLET_TOKEN = os.getenv('PUSHBULLET_TOKEN')
 pb = Pushbullet(PUSHBULLET_TOKEN)
 
 
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
 def get_price_data() -> float | None:
     # Parameters for API call
     params = {
@@ -54,36 +56,48 @@ def get_price_data() -> float | None:
         return current_price
     except requests.exceptions.RequestException as e:
         logging.exception(f'An error occurred: {e}')
-        raise e
+        raise
     except ValueError as e:
         logging.exception(f'JSON error: {e}')
-        raise e
+        raise
+
+
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(10))
+def write_price_data(data: list) -> None:
+    logging.info(f'Appending data to: {SPREADSHEET_ID}')
+    google_sheets_api.append_rows(data=data, spreadsheet_id=SPREADSHEET_ID, spreadsheet_range=PRICE_DATA_SHEET)
 
 
 def main() -> None:
     try:
         logging.info(f"{'-' * 35} STARTING  EXECUTION {'-' * 35}")
 
-        pb.push_note(title='CryptoStatsApp', body='Script Started')
+        current_price = None
 
-        current_price = get_price_data()
-
-        # Get date info
-        now = datetime.datetime.now()
-        current_date = now.strftime('%Y-%m-%d')
-        current_time = now.strftime('%H:%M:%S')
-        time_zone = get_localzone().tzname(now)
-
-        # Append most recent price to Price_Data Google Sheet
-        data = [current_date, current_time, time_zone, current_price]
         try:
-            logging.info(f'Appending data to: {SPREADSHEET_ID}')
-            google_sheets_api.append_rows(data=data, spreadsheet_id=SPREADSHEET_ID, spreadsheet_range=PRICE_DATA_SHEET)
-        except Exception as e:
-            msg = f'Error appending to sheet - Range: {PRICE_DATA_SHEET} - Data: {data} - Error: {e}'
+            current_price = get_price_data()
+        except RetryError as e:
+            msg = f'Error getting price data: {e}'
             logging.error(msg)
             pb.push_note(title='CryptoStatsApp Error', body=msg)
-            return
+
+        if current_price is not None:
+            # Get date info
+            now = datetime.datetime.now()
+            current_date = now.strftime('%Y-%m-%d')
+            current_time = now.strftime('%H:%M:%S')
+            time_zone = get_localzone().tzname(now)
+
+            # Append most recent price to Price_Data Google Sheet
+            data = [current_date, current_time, time_zone, current_price]
+
+            try:
+                write_price_data(data=data)
+            except RetryError as e:
+                msg = f'Error appending to sheet - Range: {PRICE_DATA_SHEET} - Data: {data} - Error: {e}'
+                logging.error(msg)
+                pb.push_note(title='CryptoStatsApp Error', body=msg)
+
     except Exception as e:
         logging.exception(f'Unhandled exception: {e}')
     finally:
@@ -95,6 +109,7 @@ def main() -> None:
 if __name__ == "__main__":
     try:
         # main()
+        pb.push_note(title='CryptoStatsApp', body='Script Started')
         schedule.every().hour.at('01:00').do(main)
         while True:
             schedule.run_pending()
